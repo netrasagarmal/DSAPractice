9e1316376bdefc97fb8d28293bdb6b596b3c3e10
Sagar Patil <55647831+netrasagarmal@users.noreply.github.com>
2022-02-14 23:30:36 +0530

    Added solution - LeetHub

---
+++ Sort String - GFG/sort-string.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string s1;
+	    cin>>s1;
+	    sort(s1.begin(),s1.end());
+	    cout<<s1<<endl;
+	}
+	return 0;
+}
